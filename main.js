a0b76bd977ced3ebae7857d717774ef142999d2f
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-02 14:57:35 -0400

    quiz complete

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function isHot() {
+function isHot(temp) {
+return  temp >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.' ;
 
 }
 
 
-function helloThere() {
-
+function helloThere(personName) {
+return personName.length <= 5 ? 'Hello, ' + personName : 'Hi, ' + personName;
 }
 
 
-function goodbyeYou() {
-
-}
+function goodbyeYou(personName) {
+return personName ? 'Goodbye, ' + personName : 'Goodbye, stranger';
+} 
 
 
 module.exports = {
