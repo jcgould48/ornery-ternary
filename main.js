f8d8db4fdbcd6987f3bd3c7ad42a48fd5aabc5e1
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-02 15:39:22 -0400

    notes

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 function isHot(temp) {
 return  temp >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.' ;
-
 }
-
+//function isHot(temp) {
+//  const weatherReport = temp >=80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.' ;
+//  return  weatherReport;
+//  }
 
 function helloThere(personName) {
 return personName.length <= 5 ? 'Hello, ' + personName : 'Hi, ' + personName;
 }
 
-
 function goodbyeYou(personName) {
 return personName ? 'Goodbye, ' + personName : 'Goodbye, stranger';
 } 
+
+//function goodbyeYou(personName) {
+//  return personName === undefined/*doesn't exist*/ ? 'Goodbye, stranger' : 'Goodbye, ' + personName;
+//  return (typeof personName !== 'string')
+//} 
 
 
 module.exports = {
